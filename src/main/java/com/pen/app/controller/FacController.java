310abd83e86c46756a4820ecb6b4449884eb6ab9
JINJIN <jinjin_no.1@kakao.com>
2023-05-15 19:32:07 +0900

    미진확인

---
+++ src/main/java/com/pen/app/controller/FacController.java
@@ -0,0 +1,11 @@
+package com.pen.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/fac")
+public class FacController {
+	
+
+}
