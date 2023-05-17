a4e79577e6bed80780d73e14efb0cf2c751f72ef
san1313 <skan134679@gmail.com>
2023-05-17 19:19:21 +0900

    asd

--- src/main/java/com/pen/app/config/SecurityConfig.java
+++ src/main/java/com/pen/app/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.pen.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -29,7 +31,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	http.authorizeHttpRequests((requests) ->
 	requests
-	.antMatchers("/" ,"/top", "/logout", "/login", "/**").permitAll()
+	.antMatchers("/logout", "/login").permitAll()
 	.antMatchers("/admin/**").hasAuthority("ADMIN")
 	.anyRequest().authenticated())
 	.formLogin(login->
@@ -48,5 +50,10 @@ public class SecurityConfig {
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
 		return (web) -> web.ignoring().antMatchers("/dist/**", "/plugins/**");
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		 return authenticationConfiguration.getAuthenticationManager();
 	}
 }
--- src/main/java/com/pen/app/controller/CommonController.java
+++ src/main/java/com/pen/app/controller/CommonController.java
@@ -1,10 +1,20 @@
 package com.pen.app.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.pen.app.user.UserVO;
 import com.pen.app.user.service.impl.UserServiceImpl;
@@ -16,6 +26,7 @@ import com.pen.app.user.service.impl.UserServiceImpl;
 public class CommonController {
 	
 	@Autowired UserServiceImpl userService;
+	@Autowired AuthenticationManager authenticationManager;
 	
 	@GetMapping(value = {"/top", "/"})
 	public String top() {
@@ -23,7 +34,11 @@ public class CommonController {
 	}
 	
 	@GetMapping("/modifyUser")
-	public String modifyUserForm() {
+	public String modifyUserForm(Model model, Principal principal) {
+		UserVO vo = new UserVO();
+		vo.setEmpId(principal.getName());
+		vo = userService.getUser(vo);
+		model.addAttribute("vo",vo);
 		return "/com/modifyUser";
 	}
 	
@@ -33,16 +48,25 @@ public class CommonController {
 	}
 	
 	@PostMapping("/modify")
-	public String modifyUser(UserVO vo) {
+	public ResponseEntity<HttpStatus> modifyUser(@RequestBody UserVO vo, HttpSession session) { 
 		System.out.println(vo.getPassword());
 		if (vo.getPassword()!="") {
 			vo.setEmpPw(new BCryptPasswordEncoder().encode(vo.getPassword()));
 		}
 		userService.modifyUser(vo);
-		System.out.println(vo);
-		return "/com/modifyUser";
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
 	}
 	
+	@GetMapping("/admin/userManage")
+	public String userManage(){
+		
+		return "/com/userManage";
+	}
+	
+	@GetMapping("/test")
+	public void test() {
+		
+	}
 //	@GetMapping("/user/user")
 //	public void user(@AuthenticationPrincipal Principal uservo, HttpSession session) {
 //		String sessionLoginId = (String)session.getAttribute("loginId");
