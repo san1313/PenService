20b70670ac711088e560910a76b1ecbebf1fd79b
joonho <nj7001@naver.com>
2023-05-18 19:33:33 +0900

    오늘은 깃허브도 확인하고 갑니다...ㅠ

--- src/main/java/com/pen/app/mak/vo/PlanVO.java
+++ src/main/java/com/pen/app/mak/vo/PlanVO.java
@@ -4,6 +4,53 @@ import lombok.Data;
 
 @Data
 public class PlanVO {
+	//상세계획
+	String pdCode; 
+	String planState; 
+	int planQnt;    
+	String planCode; 
+	//계획
+	String planNm; 
+	String planDate;         
+	String clsDate;         
+	String planType;  
+	String makEmpNum;
+	//연결
+	int connectCode;        
+	String ordDetCode; 
+	String contDetCode; 
+	int priority;    
+	String dclsDate;
+	//계약 상세
+	String contCode;  
+	String prodCode;  
+	String prodName;  
+	int contDetPrice;        
+	int contDetQuan;        
+	String contDetRelease;          
+	String contDetProgress; 
 	
+	//계약
+	String accCode;  
+	String accName;  
+	String contDate;          
+	String contDueDate;          
+	String contProgress; 
+	String contNote; 
+	String empNum;  
 	
+	//계약명
+	String contNm;
+	//주문
+	String ordCode;  
+	String ordDate;          
+	String ordDueDate;          
+	String ordProgress; 
+	String ordNote; 
+	
+	//주문 상세
+	int ordDetPrice;        
+	int ordDetQuan;        
+	String ordDetProgress; 
+
 }
