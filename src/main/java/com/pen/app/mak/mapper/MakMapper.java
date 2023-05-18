4ac8e83bfc4caf13cee9c31b86cd99a2c3537d1b
joonho <nj7001@naver.com>
2023-05-18 19:39:32 +0900

    Revert "오늘은 깃허브도 확인하고 갑니다...ㅠ"
    This reverts commit 20b70670ac711088e560910a76b1ecbebf1fd79b.

--- src/main/java/com/pen/app/mak/mapper/MakMapper.java
+++ src/main/java/com/pen/app/mak/mapper/MakMapper.java
@@ -1,16 +1,8 @@
 package com.pen.app.mak.mapper;
 
-import java.util.List;
-
 import com.pen.app.mak.vo.MakVO;
-import com.pen.app.mak.vo.PlanVO;
 
 public interface MakMapper {
 	MakVO getMak(MakVO vo);
 	MakVO setMak(MakVO vo);
-	List<PlanVO> getPlanList();
-	List<PlanVO> getPlanning(String contDetCode);
-	List<PlanVO> getCont();
-	List<PlanVO> getConnect(String contDetCode);
-	List<PlanVO> getContr(String ContDetCode);
 }
