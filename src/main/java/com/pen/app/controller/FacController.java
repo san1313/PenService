bda81c686ba83bc9e3d442d7cfb34a0293a30431
JINJIN <jinjin_no.1@kakao.com>
2023-05-18 19:35:04 +0900

    공정코드공정명연결

--- src/main/java/com/pen/app/controller/FacController.java
+++ src/main/java/com/pen/app/controller/FacController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pen.app.fac.mapper.FacMapper;
+import com.pen.app.fac.vo.FacConnProcVO;
 import com.pen.app.fac.vo.FacInfoVO;
 
 
@@ -34,4 +36,28 @@ public class FacController {
 		System.out.println(list);
 		return list;
 		}
+	 	
+	 	@ResponseBody
+	 	@GetMapping("/proclistAjax")
+	 	public List<FacConnProcVO> proclist(){
+	 		List<FacConnProcVO> list = dao.getproclist();
+	 		return list;
+	 	}
+	 	
+	 	
+	 	
+	 	@ResponseBody
+	 	@RequestMapping("/procminilist")
+		public List<FacConnProcVO> proclistAjax(@RequestParam String result){
+	 		String data1 = "%";
+	 		result += "%";
+	 		data1 +=result;
+	 		System.out.println(result);
+		List<FacConnProcVO> list = dao.getprocminilist(data1);
+		System.out.println(list);
+		return list;
+		}
+	 	
+	 	
+	 	
 }
--- src/main/java/com/pen/app/fac/mapper/FacMapper.java
+++ src/main/java/com/pen/app/fac/mapper/FacMapper.java
@@ -2,13 +2,16 @@ package com.pen.app.fac.mapper;
 
 import java.util.List;
 
+import com.pen.app.fac.vo.FacConnProcVO;
 import com.pen.app.fac.vo.FacInfoVO;
 
 
 public interface FacMapper {
 	
 	public List<FacInfoVO> getList(); //전체조회
+	public List<FacConnProcVO> getproclist();//공정코드목록
+	public List<FacConnProcVO> getprocminilist(String keyword); //공정코드조회
+	
 	public void insert(FacInfoVO vo); //정보등록
 	public FacInfoVO getFac(FacInfoVO vo); //단건조회
-	
-}
+	}
