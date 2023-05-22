baec614fef03aab4faae9c6c6d20c9988c20a8c1
JINJIN <jinjin_no.1@kakao.com>
2023-05-22 09:18:59 +0900

    등록 관리 분리완료

--- src/main/java/com/pen/app/fac/controller/FacController.java
+++ src/main/java/com/pen/app/fac/controller/FacController.java
@@ -17,45 +17,53 @@ import com.pen.app.fac.vo.FacInfoVO;
 @Controller
 @RequestMapping("/fac")
 public class FacController {
-	
-	@Autowired FacMapper dao;
-	
-	 @GetMapping("/management")
-	    public void management() {
-		 	
-	    }
-	 @GetMapping("/register")
-	    public void register() {
-		 	
-	    }
-	 	
-	 	@ResponseBody
-		@GetMapping("/infoListAjax")
-		public List<FacInfoVO> infoListAjax(){
-		List<FacInfoVO> list = dao.getList();
-		System.out.println(list);
-		return list;
-		}
-	 	
-	 	@ResponseBody
-	 	@GetMapping("/proclistAjax")
-	 	public List<FacConnProcVO> proclist(){
-	 		List<FacConnProcVO> list = dao.getproclist();
-	 		return list;
-	 	}
-	 	
-	 	
-	 	
-	 	@ResponseBody
-	 	@RequestMapping("/procminilist")
-		public List<FacConnProcVO> proclistAjax(@RequestParam String result){
-	 		String data1 = "%";
-	 		result += "%";
-	 		data1 +=result;
-	 		System.out.println(result);
-		List<FacConnProcVO> list = dao.getprocminilist(data1);
-		System.out.println(list);
-		return list;
-		}
-	 	
+   
+   @Autowired FacMapper dao;
+   
+    @GetMapping("/management") //설비관리 페이지
+       public void management() {
+      
+       }
+    @GetMapping("/register") //설비등록 페이지 
+       public void register() {
+          
+       }
+    
+    @GetMapping("/confirmRegister") //점검등록 페이지 
+       public void confirmRegister() {
+          
+       }
+    
+   
+    
+    
+       @ResponseBody
+      @GetMapping("/infoListAjax") //설비전체리스트
+      public List<FacInfoVO> infoListAjax(){
+      List<FacInfoVO> list = dao.getList();
+      System.out.println(list);
+      return list;
+      }
+       
+       @ResponseBody
+       @GetMapping("/proclistAjax") //공정전체리스트
+       public List<FacConnProcVO> proclist(){
+          List<FacConnProcVO> list = dao.getproclist();
+          return list;
+       }
+       
+       @ResponseBody
+       @RequestMapping("/procminilist") //공정명단건검색 리스트
+      public List<FacConnProcVO> proclistAjax(@RequestParam String result){
+          String data1 = "%";
+          result += "%";
+          data1 +=result;
+          System.out.println(result);
+      List<FacConnProcVO> list = dao.getprocminilist(data1);
+      System.out.println(list);
+      return list;
+      }
+       
+       
+       
 }
--- src/main/java/com/pen/app/fac/mapper/FacMapper.java
+++ src/main/java/com/pen/app/fac/mapper/FacMapper.java
@@ -7,10 +7,16 @@ import com.pen.app.fac.vo.FacInfoVO;
 
 
 public interface FacMapper {
-	public List<FacInfoVO> getList(); //전체조회
-	public List<FacConnProcVO> getproclist();//공정코드목록
-	public List<FacConnProcVO> getprocminilist(String keyword); //공정코드조회
-	
-	public void insert(FacInfoVO vo); //정보등록
-	public FacInfoVO getFac(FacInfoVO vo); //단건조회
-	}
+   
+   public List<FacInfoVO> getList(); //전체조회
+   public List<FacConnProcVO> getproclist();//공정코드목록
+   public List<FacConnProcVO> getprocminilist(String keyword); //공정코드조회
+   
+   public void insert(FacInfoVO vo); //설비등록
+   public void insertSelectKey(FacInfoVO vo); 
+   
+   
+   
+   public FacInfoVO getFac(String facCode); //단건조회
+   
+   }
