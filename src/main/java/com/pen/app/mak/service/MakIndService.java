b2197ccd943ee8bbc80fb9537bc061b91880b285
joonho <nj7001@naver.com>
2023-05-26 17:56:16 +0900

    0526 jh

--- src/main/java/com/pen/app/mak/service/MakIndService.java
+++ src/main/java/com/pen/app/mak/service/MakIndService.java
@@ -2,9 +2,9 @@ package com.pen.app.mak.service;
 
 import java.util.List;
 
-import com.pen.app.mak.vo.MakVO;
+import com.pen.app.mak.vo.PlanVO;
 
 
 public interface MakIndService {
-	List<MakVO> planning();
+	List<PlanVO> planning();
 }
