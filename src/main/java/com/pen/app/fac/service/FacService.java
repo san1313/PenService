710ba6750098a161440b9a4961b56e46b99e97d9
JINJIN <jinjin_no.1@kakao.com>
2023-05-30 17:46:28 +0900

    점검등록다시하기

--- src/main/java/com/pen/app/fac/service/FacService.java
+++ src/main/java/com/pen/app/fac/service/FacService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.mybatis.spring.annotation.MapperScan;
 
+import com.pen.app.fac.vo.FacConfirmVO;
 import com.pen.app.fac.vo.FacConnProcVO;
 import com.pen.app.fac.vo.FacInfoListVO;
 import com.pen.app.fac.vo.FacInfoVO;
@@ -13,14 +14,17 @@ public interface FacService {
 
 	
 	public List<FacConnProcVO> getproclist();//공정코드목록
-	public List<FacConnProcVO> getprocminilist(String keyword); //공정코드조회
+	public List<FacConnProcVO> getprocminilist(String keyword); //공정명으로 공정코드조회
+	
 	public List<FacConnProcVO> getFacConnProcList(String facCode); //설비별 공정조회
 	public int facRegister(FacInfoVO vo); //설비등록
 	public int insertFacList(FacInfoListVO list); //설비+공정등록
+	public int insertConfirmList(FacConfirmVO list); //점검등록
 	public int facUpdate(FacInfoListVO list); //설비수정
 	public int facDelete(FacInfoVO vo); //설비삭제
 	   
 	  
 	public FacInfoVO getFac(String facCode); //단건조회
+	public List<FacInfoVO> getfacminilist(String keyword);//설비명으로 설비코드조회
 	
 }
